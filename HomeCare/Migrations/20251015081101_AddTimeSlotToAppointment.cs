5e8b135c520a268e9af9047b306723147c5398f5
porridge86 <emyuand@gmail.com>
2025-10-21 15:35:58 +0200

    commit

---
+++ HomeCare/Migrations/20251015081101_AddTimeSlotToAppointment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeCare.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimeSlotToAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CustomCategory",
+                table: "Appointments",
+                newName: "TimeSlot");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TimeSlot",
+                table: "Appointments",
+                newName: "CustomCategory");
+        }
+    }
+}
