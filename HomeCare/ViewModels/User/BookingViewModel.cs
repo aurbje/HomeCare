60cdafe078dfeae58cb89351eccd346a364cda00
aurbje <143406003+aurbje@users.noreply.github.com>
2025-10-26 12:03:44 +0100

    inking updates

--- HomeCare/ViewModels/User/BookingViewModel.cs
+++ HomeCare/ViewModels/User/BookingViewModel.cs
@@ -7,13 +7,13 @@ namespace HomeCare.ViewModels
         public int TimeSlotId { get; set; } = 0; //= "08:00-9:00"; <--Booking()POST
 
         public int CategoryId { get; set; } = 0;
-        public string? Notes { get; set; } // If Cateory == "OTHER", required
+        public string? Notes { get; set; } // if Cateory == "OTHER", required
 
 
         // to display
         public List<AvailableDate> AvailableDates { get; set; } = new();
         public List<Category> Categories { get; set; } = new();
 
-        public int AppointmentId { get; set; } // For edit tracking
+        public int AppointmentId { get; set; } // for edit tracking
     }
 }
