405b325fee505f2b5627edbfe8c38de34ae1a8f9
Alexander Jahr <113902703+Jahr99@users.noreply.github.com>
2025-12-02 10:14:44 +0100

    ryddet litt i pages mappen og lagt inn enkel kode i login dashboard for å teste at ting funker

---
+++ HomeCare/frontend/src/pages/User/DashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DashboardPage() {
+  return (
+    <div className="container mt-5">
+      <h2>Dashboard</h2>
+      <p>Welcome to your dashboard.</p>
+    </div>
+  );
+}
+
+export default DashboardPage;
