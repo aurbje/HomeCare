6be4501b4a584810418846ccf42d3b5b5a6a330d
aurbje <143406003+aurbje@users.noreply.github.com>
2025-12-04 09:12:51 +0100

    removing unused files

--- HomeCare/backend/HomeCare.Api/Constans/UserRoles.cs
+++
@@ -1,9 +0,0 @@
-namespace HomeCare.Constants
-{
-    public static class UserRoles
-    {
-        public const string Admin = "Admin";
-        public const string Personnel = "Personnel";
-        public const string Client = "Client";
-    }
-}
--- HomeCare/frontend/src/components/Footer.jsx
+++
@@ -1,3 +0,0 @@
-export default function Footer() {
-  return <nav>Footer placeholder</nav>;
-}
